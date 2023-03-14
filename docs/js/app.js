db0554bb79dea73eca4a73becca4a6efed02a7a3
Gimatutdinova <gimatutdinova.k@gmail.com>
2023-03-14 20:53:03 +0300

    Новостной блок - слайдер

--- docs/js/app.js
+++ docs/js/app.js
@@ -152,3 +152,13 @@ $(".slider").slick({
     autoplay: true,
     autoplaySpeed:1500,
 });
+
+const mediaQuery = window.matchMedia('(max-width: 870px)')
+if (mediaQuery.matches)  {
+    $(".slider2").slick({
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        variableWidth: true,
+        arrows: false,
+    });
+}
